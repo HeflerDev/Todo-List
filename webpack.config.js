618a9b2a157278eb328fafc4edd7dfad90b0b696
HeflerDev <hheflerdevelopment@gmail.com>
2020-09-29 13:18:51 -0300

    config/webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,15 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+      contentBase: './dist',
   },
 };
 
